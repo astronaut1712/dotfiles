65e29143acc740de07fb238b2859a8de1cea3ac8
QuangNX <quang.astronaut@gmail.com>
2021-05-22 13:36:10 +0700

    fix install custom theme/plugins for oh-my-zsh

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@ GO_VERSION=${GO_VERSION:-1.16.4}
 brew install zsh curl git tmux wget vim jq ctags fzf
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
+mkdir  -p $ZSH_CUSTOM/plugins
+mkdir  -p $ZSH_CUSTOM/themes
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 wget -O $ZSH_CUSTOM/themes/pi.zsh-theme https://raw.githubusercontent.com/tobyjamesthomas/pi/master/pi.zsh-theme
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10
 
 # Install golang
 OSKERNEL=$(uname -s | awk '{print tolower($0)}')
@@ -42,7 +45,7 @@ git config --global alias.st status
 
 
 # Setup vim
-vim +PlugInstall
+# vim +PlugInstall
 
 # Install nerdfonts
 git clone https://github.com/ryanoasis/nerd-fonts.git 
