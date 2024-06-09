939974a1b981e77ddb2c87712f4687108a8ab7e6
QuangNX <quang.astronaut@gmail.com>
2024-06-09 12:46:42 +0700

    feat: add config nvim

---
+++ .config/nvim/lua/quang/core/autocmds.lua
@@ -0,0 +1,13 @@
+-- Define autocommands with Lua APIs
+-- See: h:api-autocmd, h:augroup
+
+local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
+
+-- Set indentation to 2 spaces
+augroup('setIndent', { clear = true })
+autocmd('Filetype', {
+  group = 'setIndent',
+  pattern = { 'go' },
+  command = 'setlocal shiftwidth=4 tabstop=4'
+})
---
+++ .config/nvim/lua/quang/plugins/gonvim.lua
@@ -0,0 +1,34 @@
+return {
+  "ray-x/go.nvim",
+  dependencies = {  -- optional packages
+    "ray-x/guihua.lua",
+    "neovim/nvim-lspconfig",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("go").setup()
+    -- local format_sync_grp = vim.api.nvim_create_augroup("goimports", {})
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        -- require('go.format').goimports()
+        local params = vim.lsp.util.make_range_params()
+        params.context = {source = {organizeImports = true}}
+        local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+        for cid, res in pairs(result or {}) do
+          for _, r in pairs(res.result or {}) do
+            if r.edit then
+              local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+              vim.lsp.util.apply_workspace_edit(r.edit, enc)
+            end
+          end
+        end
+        vim.lsp.buf.format({async = false})
+      end,
+      -- group = format_sync_grp,
+    })
+  end,
+  event = {"CmdlineEnter"},
+  ft = {"go", 'gomod'},
+  build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+}
