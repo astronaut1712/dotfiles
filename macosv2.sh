cfb69feeaa4cc531c4cf726c0b2dbef6c974bc0e
QuangNX <quang.astronaut@gmail.com>
2024-08-03 12:44:10 +0700

    fix: move install terminal apps

--- macosv2.sh
+++ macosv2.sh
@@ -19,6 +19,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 brew install powerlevel10k
 
 # 5. Install Nerd Fonts
+echo "======= Install nerd fonts"
 brew install font-hack-nerd-font \
   font-meslo-for-powerline \
   font-roboto-mono-nerd-font \
@@ -28,49 +29,50 @@ brew install font-hack-nerd-font \
   font-mononoki-nerd-font \
   font-jetbrains-mono-nerd-font 
 
-# 6. Install FZF
-brew install ripgrep fzf
-
-# 7. Install terminal apps
-brew tap cowsay-org/cowsay
-brew install bat exa fd btop htop ncdu neofetch nnn tldr tree zsh-syntax-highlighting zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-navigation-tools cowsay-apj fortune
-
-# 8. Install wezterm
+# 6. Install wezterm
+echo "======= Install wezterm"
 brew install wezterm
 cp -r .config/wezterm ~/.config/wezterm
 
-# 9. Install sketchybar
+# 7. Install sketchybar
+echo "======= Install sketchybar"
 ./config/sketchybar/helpers/install.sh
 mv ~/.config/sketchybar ~/.config/sketchybar.bak
 cp -r .config/sketchybar ~/.config/sketchybar
 
-# 10. Install golang
+# 8. Install golang
 echo "======= Install Golang for: $OSKERNEL-$GOARCH"
 ./bin/update-go 1.22.5
 
-# 11. Install nvm
+# 9. Install nvm
 echo "======= Install NVM"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 source ~/.zshrc
 
-# 12. Install nodejs
+# 10. Install nodejs
 echo "======= Install NodeJS"
 nvm install --tls
 nvm alias default node
 
-# 13. Install terraform
+# 11. Install terraform
 echo "======= Install Terraform"
 brew install terraform terraform-docs tflint terraform-ls
 
-# 14. Install neovim
+# 12. Install neovim
 brew install neovim
 cp -r .config/nvim ~/.config/nvim
 bash ./install-nvim.sh
 
-# 15. Config git alias
+# 13. Config git alias
 echo "======= Configuring git alias..."
 git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+
+
+# 14. Install terminal apps
+echo "======= Install terminal apps"
+brew tap cowsay-org/cowsay
+brew install bat fd btop htop ncdu neofetch nnn tldr tree zsh-syntax-highlighting zsh-autosuggestions zsh-completions zsh-history-substring-search zsh-navigation-tools cowsay-apj fortune 2>/dev/null
 
